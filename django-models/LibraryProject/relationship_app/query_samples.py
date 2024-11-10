54003bdcab14c55b9422310c4977fe89b0c78f94
Sondos91 <sondossfwat@gmail.com>
2024-11-10 02:19:56 +0200

    test1

--- django-models/LibraryProject/relationship_app/query_samples.py
+++
@@ -1,8 +0,0 @@
-from models import Author, Book, Library, Librarian
-
-author = Author.objects.get(name= "George Orwell")
-
-books = Book.objects.all()
-
-librarian = Librarian.objects.get(Library= "Animal Farm")
-
--- django-models/LibraryProject/relationship_app/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . views import LibraryDetailView, book_list
-
-urlpatterns = [
-    path('library/', LibraryDetailView.as_view(), name='library_detail'),
-    
-    path('book_list/', book_list, name='book_list'),
-]
