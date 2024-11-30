b293074ae2ac4cd0075a0c286ae888cb01d0d697
Sondos91 <sondossfwat@gmail.com>
2024-11-30 23:06:16 +0200

    api

--- advanced-api-project/api/urls.py
+++ advanced-api-project/api/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 from . import views
+from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAuthenticated
 
 urlpatterns = [
     # Add URL patterns for your views here
     # For example:
-    path('authors/', views.AuthorListView.as_view()),
+    path('authors/', views.AuthorListView.as_view()), 
     path('authors/<int:pk>/', views.AuthorDetailView.as_view()),
     path('books/', views.BookListView.as_view()),
     path('books/<int:pk>/', views.BookDetailView.as_view()),
