75b22691b42950602d314200d897880b57fdb0aa
Sondos91 <sondossfwat@gmail.com>
2024-11-30 20:43:59 +0200

    api

--- advanced_api_project/api/serializers.py
+++ advanced_api_project/api/serializers.py
@@ -8,11 +8,15 @@ class BookSerializer(serializers.ModelSerializer):
         fields = '__all__'
 
         
-        
-class AuthorSerializer(serializers.ModelSerializer):
-        books = BookSerializer(many=True, read_only=True)
+class BookSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Book
+        fields = '__all__'
 
-  class Meta:
+class AuthorSerializer(serializers.ModelSerializer):
+    books = BookSerializer(many=True, read_only=True)
+
+    class Meta:
         model = Author
         fields = ['name', 'books']
 
