9911619441419b5ed3c9ecde91beeff04634e3d5
Sondos91 <sondossfwat@gmail.com>
2024-11-30 20:53:16 +0200

    api

--- advanced_api_project/advanced_api_project/urls.py
+++ advanced_api_project/advanced_api_project/urls.py
@@ -20,3 +20,10 @@ from django.urls import path
 urlpatterns = [
     path('admin/', admin.site.urls),
 ]
+from django.contrib import admin
+from django.urls import include, path
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('api/', include('api.urls')),  # Add this line
+]
--- advanced_api_project/api/views.py
+++ advanced_api_project/api/views.py
@@ -1,3 +1,29 @@
 from django.shortcuts import render
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from .models import Author, Book
+from .serializers import AuthorSerializer, BookSerializer
 
-# Create your views here.
+class AuthorListView(APIView):
+    def get(self, request):
+        authors = Author.objects.all()
+        serializer = AuthorSerializer(authors, many=True)
+        return Response(serializer.data)
+
+class AuthorDetailView(APIView):
+    def get(self, request, pk):
+        author = Author.objects.get(pk=pk)
+        serializer = AuthorSerializer(author)
+        return Response(serializer.data)
+
+class BookListView(APIView):
+    def get(self, request):
+        books = Book.objects.all()
+        serializer = BookSerializer(books, many=True)
+        return Response(serializer.data)
+
+class BookDetailView(APIView):
+    def get(self, request, pk):
+        book = Book.objects.get(pk=pk)
+        serializer = BookSerializer(book)
+        return Response(serializer.data)
