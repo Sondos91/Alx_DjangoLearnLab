f5ebfe0d29f3efed7399b894af5c7e858e3fce07
Sondos91 <sondossfwat@gmail.com>
2024-11-30 23:04:57 +0200

    api

--- advanced-api-project/api/urls.py
+++ advanced-api-project/api/urls.py
@@ -12,8 +12,8 @@ urlpatterns = [
 
 urlpatterns = [
     path('books/', views.BookListView.as_view()),
-    path('books/<int:pk>/', views.BookDetailView.as_view()),
-    path('books/create/', views.BookCreateView.as_view()),
-    path('books/<int:pk>/update/', views.BookUpdateView.as_view()),
-    path('books/<int:pk>/delete/', views.BookDeleteView.as_view()),
+    path("booksdetail", views.BookDetailView.as_view()),
+    path("books/create",views.BookCreateView.as_view()),
+    path("books/update", views.BookUpdateView.as_view()),
+    path("books/delete", views.BookDeleteView.as_view()),
 ]
