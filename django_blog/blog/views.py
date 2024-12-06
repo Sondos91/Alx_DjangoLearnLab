83a4bd20c98f36b23f343df28337b1b49b40e4d6
Sondos91 <sondossfwat@gmail.com>
2024-12-06 02:54:12 +0200

    blogs

--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -6,6 +6,7 @@ from .models import Post
 from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 from django.urls import reverse_lazy
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 
