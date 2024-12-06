ce74b912b3709299de8cc932e95da625917a6088
Sondos91 <sondossfwat@gmail.com>
2024-12-07 00:21:44 +0200

    blogs

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -19,5 +19,5 @@ urlpatterns = [
     path('comment/<int:pk>/update/', views.CommentUpdateView.as_view() , name='comment_update'),
     path('comment/<int:pk>/delete/', views.CommentDeleteView.as_view() , name='comment_delete'),
     path('search/',views.PostSearchView.as_view(), name='post_search'),
-    path('tags/<slug:tag_slug>/", "PostByTagListView.as_view(), name="post_by_tag'),
+    path('tags/<slug:tag_slug>/',views.PostByTagListView.as_view(),name='tags'),
 ]
--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -195,3 +195,17 @@ class PostSearchView(ListView):
                 Q(tags__name__icontains=query).distinct())
         else:
             return Post.objects.all()
+
+class PostByTagListView(ListView):
+    model = Post
+    template_name = 'blog/post_list.html'
+    context_object_name = 'posts'
+
+    def get_queryset(self):
+        tag = self.kwargs.get('tag')
+        return Post.objects.filter(tags__name__in=[tag])
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['tag'] = self.kwargs.get('tag')
+        return context
