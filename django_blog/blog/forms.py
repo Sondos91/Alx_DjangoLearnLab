1152655fe16d79433fd67e915e79ec77516ee1d8
Sondos91 <sondossfwat@gmail.com>
2024-12-07 00:12:43 +0200

    blogs

--- django_blog/blog/forms.py
+++ django_blog/blog/forms.py
@@ -1,6 +1,7 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
+from taggit.forms import TagWidget
 from blog.models import Post , Comment
 
 class SignUpForm(UserCreationForm):
@@ -22,6 +23,9 @@ class PostForm(forms.ModelForm):
     class Meta:
         model = Post
         fields = ['title', 'content', 'tag']
+        widgets = {
+            'tag': TagWidget(),
+        }
 
 class CommentForm(forms.ModelForm):
     class Meta:
