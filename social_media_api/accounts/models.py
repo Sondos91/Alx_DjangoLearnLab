99b78e28e6a6e909c525f7ce81bbcee0cd70cb11
Sondos91 <sondossfwat@gmail.com>
2024-12-13 18:56:26 +0200

    social media

--- social_media_api/accounts/models.py
+++ social_media_api/accounts/models.py
@@ -1,9 +1,12 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 
+
 # Create your models here.
 
 class User(AbstractUser):
     bio = models.TextField(max_length=500, blank=True)
     profile_picture = models.ImageField(upload_to='images', blank=True, null=True)
-    followers = models.ManyToManyField("self", symmetrical=False, blank=True)
+    followers = models.ManyToManyField("self", symmetrical=False, blank=True , related_name="followed_by")
+    groups = models.ManyToManyField("auth.Group", blank=True, related_name="customuser_set")
+    user_permissions = models.ManyToManyField("auth.Permission", blank=True, related_name="customuser_set")
--- social_media_api/accounts/serializers.py
+++ social_media_api/accounts/serializers.py
@@ -1,9 +1,14 @@
 from rest_framework import serializers
-from .models import User
+from .models import User , Token
 
 
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
+        fields = '__all__'
+
+class TokenSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Token
         fields = '__all__'
 
